aeb38cd72b3eaa1bcc30bf05f4bd12b00e33d646
Luka Miletic <lukamilecki998@gmail.com>
2021-01-18 15:54:10 +0100

    Obrisan skelet projekta koji je inicijalno dodat.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/uros/Desktop/RG-TargetShooting
+# Install script for directory: /home/luka/Desktop/projekti/RG-TargetShooting
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,16 +37,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/uros/Desktop/RG-TargetShooting/cmake-build-debug/libs/glad/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/uros/Desktop/RG-TargetShooting/cmake-build-debug/libs/imgui/cmake_install.cmake")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -55,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/uros/Desktop/RG-TargetShooting/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/luka/Desktop/projekti/RG-TargetShooting/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- cmake-build-debug/configuration/root_directory.h
+++
@@ -1,1 +0,0 @@
-const char * logl_root = "/home/uros/Desktop/RG-TargetShooting";
